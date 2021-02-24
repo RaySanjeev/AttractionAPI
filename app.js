bcbe9732e4f528c335f68cde4f07cf301f99b0c1
Sanjeev Ray <sanjeevat1996@gmail.com>
2021-02-24 14:05:49 +0530

    Refactoring the code

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 
 const attractionRouter = require('./routes/attractionRouter');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
+// COOKIE PARSER
+app.use(cookieParser());
+
 app.enable('trust proxy');
 
-// SERVING STATIC FILES
-app.use(express.static(path.join(__dirname, 'public')));
+// Rate limiter
+const limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Please try again after an hour.',
+});
+app.use('/api', limiter);
 
 // Adding incoming request data to the req.body
 app.use(express.json({ limit: '10kb' })); // JSON Parser
@@ -22,6 +30,11 @@ app.use(compression());
 
 // ROUTES
 app.use('/api/v1/attractions', attractionRouter);
+
+// ALL OTHER UNIDENTIFIED ROUTES
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
 
 // GLOBAL ERROR HANDLER
 app.use(globalErrorHandler);
